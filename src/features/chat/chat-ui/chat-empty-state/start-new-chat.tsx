f1977f13d7fd09b5ef08b90078a5abfee191ec97
formaz104 <107877504+formaz104@users.noreply.github.com>
2023-11-15 15:02:20 +0100

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -12,12 +12,12 @@ export const StartNewChat: FC<Prop> = (props) => {
       <div className="col-span-2 gap-5 flex flex-col flex-1">
         <img src="/ai-icon.png" className="w-36" />
       </div>
-      <Card className="col-span-3 flex flex-col gap-5 p-5 ">
+      <Card className="flex flex-col gap-5 p-5 ">
         <Typography variant="h4" className="text-primary">
           {AI_NAME}
         </Typography>
         <div className="flex flex-col gap-2">
-          <p className="overflow-auto">
+          <p className="">
             Welcome to {AI_NAME}. <br />
             En utilisant ChatGPT MH, veuillez prendre en compte les points suivants :<br />
 
